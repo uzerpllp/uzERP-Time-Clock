50056a9dd98ac05caeb81a92762be904c6745785
Ben Everard <hello@beneverard.co.uk>
2012-02-20 14:10:26 +0000

    Quick fix to style settings page

--- application/views/settings/index.php
+++ application/views/settings/index.php
@@ -1,27 +1,40 @@
 <?php $this->load->view('common/header'); ?>
 
-	<h2>Settings</h2>
-	
-	<?php if (isset($form_valid) && $form_valid === FALSE) : ?>
-		<ul class="errors">
-			<?php echo validation_errors(); ?>
-		</ul>
-	<?php endif; ?>
+<div class="content">
 
-	<form action="" method="post">
-		<ul>
-			<li>
+	<div class="page-header">
+		<h1>Time Clock</h1>
+	</div>
+	
+	<div class="row">
+		<div class="span10">
+			
+			<h2>Settings</h2>
+			
+			<?php if (isset($form_valid) && $form_valid === FALSE) : ?>
+				<ul class="errors">
+					<?php echo validation_errors(); ?>
+				</ul>
+			<?php endif; ?>
+		
+			<form action="" class="form-vertical" method="post">
+				
 				<label>Minutes between swipe:</label>
 				<input type="text" name="minutes_between_swipe" value="<?php echo set_value('minutes_between_swipe', $minutes_between_swipe); ?>" />
-			</li>
-			<li>
+				
 				<label>Maximum shift length (hours):</label>
 				<input type="text" name="maximum_shift_length" value="<?php echo set_value('maximum_shift_length', $maximum_shift_length); ?>" />
-			</li>
-			<li>
-				<input type="submit" />
-			</li>
-		</ul>
-	</form>
+				
+				<br />
+				
+				<input type="submit" value="Save Settings" class="btn" />
+				
+			</form>
+		
+		</div>
+	</div>
+</div>
+
+
 
 <?php $this->load->view('common/footer'); ?>
